e72849427e901fd57f9ccbd45d1165d770313f31
Everton Lacerda <everton3b@hotmail.com>
2020-04-01 18:18:02 -0300

    create and config auth with express-jwt

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    sercret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'AS&98766756GHGTRQBVNBA6YHUYAYDD55',
+    secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'AS&98766756GHGTRQBVNBA6YHUYAYDD55',
     api: process.env.NODE_ENV === 'production' ? 'https//api/loja-teste.com' :  'https//localhost:3000',
     loja: process.env.NODE_ENV === 'production' ? 'https//loja-teste.com' :  'https//localhost:8000',
 }
